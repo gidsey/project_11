506a22b482adf11e8cdbed9f36ad03d28d5e48b7
gidsey <chris@chrisguy.co>
2020-02-15 13:11:10 +0000

    serializers added

--- backend/pugorugh/serializers.py
+++ backend/pugorugh/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
+from . import models
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -16,3 +18,26 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
+
+
+class DogSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'name',
+            'image_filename',
+            'breed',
+            'age',
+            'gender',
+            'size',
+        )
+        model = models.Dog
+
+
+class UserPrefSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'dog',
+            'status',
+        )
