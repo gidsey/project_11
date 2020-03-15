28852880690eb3a12dc2ae3f58d7f947fee81cb5
gidsey <chris@chrisguy.co>
2020-03-15 10:52:00 +0000

    api tests (wip)

--- backend/pugorugh/tests/test_api.py
+++ backend/pugorugh/tests/test_api.py
@@ -1,2 +1,15 @@
-from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from pugorugh import models
+
+
+class GetNextTests(APITestCase):
+    def test_undecided(self):
+        """
+        Ensure unauthorised access is denied.
+        """
+        url = reverse('get-next', kwargs={'status': 'undecided', 'pk': -1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
