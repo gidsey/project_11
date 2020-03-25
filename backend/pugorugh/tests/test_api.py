38fd759816a679dccc1c20a6724bdf18d2fe6b08
gidsey <chris@chrisguy.co>
2020-03-25 18:05:17 +0000

    Tests for Add and Delete Dog methods

--- backend/pugorugh/tests/test_api.py
+++ backend/pugorugh/tests/test_api.py
@@ -410,3 +410,59 @@ class UserPrefsTests(APITestCase):
                 'size': 's,l',
             }
         )
+
+
+class AddDeleteTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('david', 'holmes@davidholmes.com', 'mymatepaul')
+
+        self.dog_1 = models.Dog.objects.create(
+            name='Snoopy',
+            image_filename='snoopy.jpg',
+            breed="Beagle",
+            age=46,
+            gender='m',
+            size='s')
+
+    def test_add_dog(self):
+        """
+        Ensure that a dog can be added via the API.
+        """
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+        url = reverse('add-dog')
+        data = {
+            'name': 'Gnasher',
+            'image_filename': 'gnasher.jpg',
+            'breed': 'Wire-haired Tripehound',
+            'age': 76,
+            'gender': 'm',
+            'size': 'm'}
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertJSONEqual(
+            str(response.content, encoding='utf8'),
+            {
+                'id': 2,
+                'name': 'Gnasher',
+                'image_filename': 'gnasher.jpg',
+                'breed': 'Wire-haired Tripehound',
+                'age': 76,
+                'gender': 'm',
+                'size': 'm'
+            }
+        )
+
+    def test_delete_dog(self):
+        """
+        Ensure that a dog can be deleted via the API.
+        """
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+        url = reverse('delete-dog', kwargs={'pk': 1})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
